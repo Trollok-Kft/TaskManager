fecd606bb702f55908351a1b5cecfb70db364971
bibor-01 <balogh01biborka@gmail.com>
2021-12-03 13:46:25 +0100

    keresés a TASK ok között

--- js/js.js
+++ js/js.js
@@ -2,22 +2,32 @@ $(function () {
     const ajax=new AjaxHivas();
 
     const eleresiUt="http://localhost:3000/tasks?_expand=user";
+    const eleresiUt2="http://localhost:3000/tasks";
+
 
     ajax.getAjax(eleresiUt,adatKiiras);
 
 
-    const szuloElem = $("article");
-    const sablonElem = $(".taskSablon");
+    const szuloElem = $(".task");
+    const sablonElem = $("#sablon .taskSablon");
+    szuloElem.empty();
+    sablonElem.show();
 
-    sablonElem.remove();
+    
 
     function adatKiiras(tasks){
         tasks.forEach(function (adat) {
             let ujElem = sablonElem.clone().appendTo(szuloElem);
             const task = new Task(ujElem, adat);
-            console.log(adat);
+            //console.log(adat);
         });
+        sablonElem.hide();
 }
+$("#keres").on("keyup", function(){
+    console.log($("#keres"));
+    let mezoszerint = eleresiUt2 + "?q="+$("#keres").val();
+    ajax.getAjax(mezoszerint, adatKiiras);
+  });
 
 });
 
