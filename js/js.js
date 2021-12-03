8fd3326fed59127b610a7d8f0b3a5291da311680
bartuczpetra <bartucz.petra@gmail.com>
2021-12-03 13:43:11 +0100

    TorlesKezdet

--- js/js.js
+++ js/js.js
@@ -1,9 +1,10 @@
 $(function () {
-    const ajax=new AjaxHivas();
+    const ajax = new AjaxHivas();
 
-    const eleresiUt="http://localhost:3000/tasks?_expand=user";
+    const eleresiUt = "http://localhost:3000/tasks?_expand=user";
+    const torlesEleresiUt = "http://localhost:3000/tasks";
 
-    ajax.getAjax(eleresiUt,adatKiiras);
+    ajax.getAjax(eleresiUt, adatKiiras);
 
 
     const szuloElem = $("article");
@@ -11,13 +12,18 @@ $(function () {
 
     sablonElem.remove();
 
-    function adatKiiras(tasks){
+    function adatKiiras(tasks) {
         tasks.forEach(function (adat) {
             let ujElem = sablonElem.clone().appendTo(szuloElem);
             const task = new Task(ujElem, adat);
             console.log(adat);
         });
-}
+    }
+    $(window).on("torlesKattintas", (event) => {
+        console.log(event.detail.id);
+        //ajax.deleteAjax(torlesEleresiUt,event.detail.id);
+        //ajax.getAjax(eleresiUt, adatKiiras);
+      });
 
 });
 
