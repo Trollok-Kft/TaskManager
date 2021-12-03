e9c1d2a9f669ee1830616e8b32826b83487c9d37
bartuczpetra <bartucz.petra@gmail.com>
2021-12-03 11:54:46 +0100

    TASKok megjelenítése

---
+++ js.js
@@ -0,0 +1,23 @@
+$(function () {
+    const ajax=new AjaxHivas();
+
+    const eleresiUt="http://localhost:3000/tasks";
+
+    ajax.getAjax(eleresiUt,adatKiiras);
+
+
+    const szuloElem = $("article");
+    const sablonElem = $(".taskSablon");
+
+    sablonElem.remove();
+
+    function adatKiiras(tasks){
+        tasks.forEach(function (adat) {
+            let ujElem = sablonElem.clone().appendTo(szuloElem);
+            const task = new Task(ujElem, adat);
+            console.log(adat);
+        });
+}
+
+});
+
